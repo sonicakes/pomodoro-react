1e4d25a07aa695d0dec519ef023f003827ccf4cd
sonia parker <sonia.parker@ping-works.com.au>
2023-11-19 13:07:00 +1100

    add react hook timer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Timer from './components/timer/Timer';
-
+import Timer from "./components/timer/Timer";
+//todo: why does it start with 25:0 instead of 25:00
 function App() {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 1500); // 25 minutes timer
   return (
     <div className="App">
-      <Timer />
+      <div>
+        <Timer expiryTimestamp={time}/>
+      </div>
     </div>
   );
 }
--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,11 +1,34 @@
+import { useTimer } from 'react-timer-hook';
 
-
-function Timer() {
-  return (
-    <div className="timer">
-      <p>I am timer</p>
-    </div>
-  );
-}
-
+function Timer({ expiryTimestamp }) {
+    const {
+      seconds,
+      minutes,
+      isRunning,
+      start,
+      pause,
+      resume,
+      restart,
+    } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called - the timer has run its course'), autoStart: false });
+  
+  
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h1>Pomodoro timer </h1>
+        <div style={{fontSize: '100px'}}>
+         <span>{minutes}</span>:<span>{seconds}</span>
+        </div>
+        <p>{isRunning ? 'Running' : 'Not running'}</p>
+        <button onClick={start}>Start</button>
+        <button onClick={pause}>Pause</button>
+        <button onClick={resume}>Resume</button>
+        <button onClick={() => {
+          // Restarts to 25 minutes timer
+          const time = new Date();
+          time.setSeconds(time.getSeconds() + 1500);
+          restart(time)
+        }}>Restart</button>
+      </div>
+    );
+  }
 export default Timer;
