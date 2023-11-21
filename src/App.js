959b53e23bbde2c85f79b929342823fa58d9773c
Sonia Parker <soniaparker1207@gmail.com>
2023-11-21 13:42:40 +1100

    add chakra comps & format time

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import Timer from "./components/timer/Timer";
-//todo: why does it start with 25:0 instead of 25:00
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+
+
 function App() {
   const time = new Date();
   time.setSeconds(time.getSeconds() + 1500); // 25 minutes timer
   return (
     <div className="App">
       <div>
-        <Timer expiryTimestamp={time}/>
+        <Tabs>
+  <TabList>
+    <Tab>Pomodoro Session</Tab>
+    <Tab>Two</Tab>
+    <Tab>Three</Tab>
+  </TabList>
+
+  <TabPanels>
+    <TabPanel>
+    <Timer expiryTimestamp={time}/>
+    </TabPanel>
+    <TabPanel>
+      <p>two!</p>
+    </TabPanel>
+    <TabPanel>
+      <p>three!</p>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
       </div>
     </div>
   );
--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,4 +1,5 @@
 import { useTimer } from 'react-timer-hook';
+import './timer.css';
 
 function Timer({ expiryTimestamp }) {
     const {
@@ -10,13 +11,14 @@ function Timer({ expiryTimestamp }) {
       resume,
       restart,
     } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called - the timer has run its course'), autoStart: false });
-  
-  
+    const formatTime = (time) => {
+      return String(time).padStart(2, "0");
+    };
     return (
       <div style={{textAlign: 'center'}}>
         <h1>Pomodoro timer </h1>
-        <div style={{fontSize: '100px'}}>
-         <span>{minutes}</span>:<span>{seconds}</span>
+        <div className="timer">
+       <span>{formatTime(minutes)}</span>:<span>{formatTime(seconds)}</span>
         </div>
         <p>{isRunning ? 'Running' : 'Not running'}</p>
         <button onClick={start}>Start</button>
